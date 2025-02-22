536aa0fa9ff4d0645269335df7970b175b2d8f50
Thành <131802697+suyttthideptrai@users.noreply.github.com>
2025-02-22 15:48:36 +0700

    fix class file get content

--- python/class_filter.py
+++ python/class_filter.py
@@ -5,9 +5,7 @@ import pandas as pd
 
 
 meta_esc_50 = "https://github.com/karolpiczak/ESC-50/blob/master/meta/esc50.csv"
-response = urllib.request.urlopen(meta_esc_50)
-with open('esc50.csv', 'wb') as file:
-    file.write(response.content)
-esc50_df = pd.read_csv('esc50.csv')
+with urllib.request.urlopen(meta_esc_50) as f:
+    esc50_df = pd.read_csv(f.read.decode('utf-8'))
 classes = esc50_df['category'].unique()
 print(classes)
