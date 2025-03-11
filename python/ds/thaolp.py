385f415947a5dcc98d7e5eb42df816743f5388d2
Thành <131802697+suyttthideptrai@users.noreply.github.com>
2025-03-11 04:04:09 +0000

    init ThaoLp.py #2

--- python/ds/thaolp.py
+++ python/ds/thaolp.py
@@ -9,6 +9,7 @@ import zipfile
 import os
 import gdown
 from file_utils import extract
+from platformdirs import user_cache_dir
 
 class ThaoLp(DataSet):
     """_summary_
@@ -16,25 +17,11 @@ class ThaoLp(DataSet):
     """
     def __init__(self):
         key = "thaoLp"
-        # Determine the dataset path
-        cache_dir = user_cache_dir()
-        self.ds_abs_path = os.path.join(cache_dir, "datasets", key)
-        # Ensure the directory exists before calling super()
-        os.makedirs(self.ds_abs_path, exist_ok=True)
         super().__init__(key)
 
     def download(self):
-        zip_url = "https://drive.google.com/uc?id=1YQrV_45hnL4EgXwiU9Yqu1HTCe-RkMFD"
-        zip_path = os.path.join(self.ds_abs_path, "Sound.zip")
-        extract_path = os.path.join(self.ds_abs_path, "ThaoLp")
+        pass
 
-        # Ensure the directory exists before downloading the file
-        os.makedirs(self.ds_abs_path, exist_ok=True)
-
-        gdown.download(zip_url, "Sound.zip", quiet=False)
-        extract("Sound.zip", extract_path)
-        return extract_path 
-    
     def init_class_names(self):
         """_summary_
         This one doesn;t have meta file so we:
@@ -43,49 +30,30 @@ class ThaoLp(DataSet):
         3. Assign created `.csv` filepath to `self.meta_sub_path`
         """
         
-        # Load folder name as class names
-        folders = [f.name for f in os.scandir(self.ds_abs_path) if f.is_dir()]
-        df = pd.DataFrame(folders, columns=["category"])
-        self.class_names = df["category"].unique()
+        # # Load folder name as class names
+        # folders = [f.name for f in os.scandir(self.ds_abs_path) if f.is_dir()]
+        # df = pd.DataFrame(folders, columns=["category"])
+        # self.class_names = df["category"].unique()
 
-        columns = ["idx", "filename", "filepath", "category"]        
-        original_ds_df = pd.DataFrame(columns=columns)
-        index = 0;
-        for category in folders:
-            sub_f_path = os.path.join(self.ds_abs_path, category)
-            sub_f_files = [f.name for f in os.scandir(sub_f_path) if f.is_file() and f.name.endswith('.wav')]
-            for f in sub_f_files:
-                file_name = f
-                file_path = os.path.join(category, f)
-                original_ds_df.loc[len(original_ds_df)] = [index, file_name, file_path, category]
-                index += 1
+        # columns = ["idx", "filename", "filepath", "category"]        
+        # original_ds_df = pd.DataFrame(columns=columns)
+        # index = 0;
+        # for category in folders:
+        #     sub_f_path = os.path.join(self.ds_abs_path, category)
+        #     sub_f_files = [f.name for f in os.scandir(sub_f_path) if f.is_file() and f.name.endswith('.wav')]
+        #     for f in sub_f_files:
+        #         file_name = f
+        #         file_path = os.path.join(category, f)
+        #         original_ds_df.loc[len(original_ds_df)] = [index, file_name, file_path, category]
+        #         index += 1
                 
-        # Init original dataset .csv meta file
-        path = write_csv_meta(original_ds_df, self.key + ".original")
-        self.meta_sub_path = path
+        # # Init original dataset .csv meta file
+        # path = write_csv_meta(original_ds_df, self.key + ".original")
+        # self.meta_sub_path = path
         
 
     def filter_by_class(self):
-        """_summary_
-        """
-        # # Load and filter the original meta file
-        df = pd.read_csv(self.meta_sub_path)
-        # Get allowed classes and their mapping
-        class_map = get_post_class_mapping(self.key)
-        # Eliminate classes not in class_map
-        df_filtered = df[df["category"].isin(class_map.keys())]
-        df = df_filtered
-        print(df.head(10))
-        
-        # # Map filtered dataframe to self.df with the right schema
-        # # self.df[C.DF_ID_COL] = range(len(df))
-        self.df[C.DF_NAME_COL] = df["filename"]
-        self.df[C.DF_PATH_COL] = df["filepath"].apply(lambda fp: os.path.join(self.ds_abs_path, fp))
-        self.df[C.DF_LENGTH_COL] = self.df[C.DF_PATH_COL].apply(get_wav_data_length)
-        self.df[C.DF_CLASS_ID_COL] = df["category"].apply(lambda original_classname: class_map[original_classname][C.CLASS_ID])
-        self.df[C.DF_CLASS_NAME_COL] = df["category"].apply(lambda original_classname: class_map[original_classname][C.CLASS_NAME])
-        self.df[C.DF_SUB_DS_NAME_COL] = self.name
-        self.df[C.DF_SUB_DS_ID_COL] = df.index
+        pass
 
     def normalize(self):
         """
@@ -94,13 +62,10 @@ class ThaoLp(DataSet):
         pass
 
     def create_meta(self):
-        df = pd.read_csv(self.meta_sub_path)
-        path = write_csv_meta(self.df, self.key + ".filtered")
-        _ = write_csv_meta(df, self.key + ".original")
-        self.filtered_meta_path = path
+        pass
     
     def get_filtered_meta_path(self):
-        return self.filtered_meta_path
+        pass
 
     def move_files(self):
         """
@@ -108,10 +73,10 @@ class ThaoLp(DataSet):
         """
         pass
     
-# def main():
-#     ds = GAD()
-#     append_empty_mapping_to_config(ds, overwrite=False)
-#     ds.hell_yeah()
+def main():
+    ds = ThaoLp() 
+    # append_empty_mapping_to_config(ds, overwrite=False)
+    ds.download()
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
