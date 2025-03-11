93b183b346ad9a0198c3659e5dd69055a2eda40d
Thành <131802697+suyttthideptrai@users.noreply.github.com>
2025-03-11 02:46:23 +0000

    init cloned thaoLp dataset

---
+++ python/ds/thaolp.py
@@ -0,0 +1,117 @@
+from ds.dataset import DataSet
+from utils.csv_utils import write_csv_meta
+from utils.json_utils import append_empty_mapping_to_config, get_post_class_mapping
+from utils.wav_utils import get_wav_data_length
+import constants as C
+import kagglehub
+import pandas as pd
+import zipfile
+import os
+import gdown
+from file_utils import extract
+
+class ThaoLp(DataSet):
+    """_summary_
+        Self colected dataset
+    """
+    def __init__(self):
+        key = "thaoLp"
+        # Determine the dataset path
+        cache_dir = user_cache_dir()
+        self.ds_abs_path = os.path.join(cache_dir, "datasets", key)
+        # Ensure the directory exists before calling super()
+        os.makedirs(self.ds_abs_path, exist_ok=True)
+        super().__init__(key)
+
+    def download(self):
+        zip_url = "https://drive.google.com/uc?id=1YQrV_45hnL4EgXwiU9Yqu1HTCe-RkMFD"
+        zip_path = os.path.join(self.ds_abs_path, "Sound.zip")
+        extract_path = os.path.join(self.ds_abs_path, "ThaoLp")
+
+        # Ensure the directory exists before downloading the file
+        os.makedirs(self.ds_abs_path, exist_ok=True)
+
+        gdown.download(zip_url, "Sound.zip", quiet=False)
+        extract("Sound.zip", extract_path)
+        return extract_path 
+    
+    def init_class_names(self):
+        """_summary_
+        This one doesn;t have meta file so we:
+        1. load folder names as class names
+        2. Make a meta file in `.csv`
+        3. Assign created `.csv` filepath to `self.meta_sub_path`
+        """
+        
+        # Load folder name as class names
+        folders = [f.name for f in os.scandir(self.ds_abs_path) if f.is_dir()]
+        df = pd.DataFrame(folders, columns=["category"])
+        self.class_names = df["category"].unique()
+
+        columns = ["idx", "filename", "filepath", "category"]        
+        original_ds_df = pd.DataFrame(columns=columns)
+        index = 0;
+        for category in folders:
+            sub_f_path = os.path.join(self.ds_abs_path, category)
+            sub_f_files = [f.name for f in os.scandir(sub_f_path) if f.is_file() and f.name.endswith('.wav')]
+            for f in sub_f_files:
+                file_name = f
+                file_path = os.path.join(category, f)
+                original_ds_df.loc[len(original_ds_df)] = [index, file_name, file_path, category]
+                index += 1
+                
+        # Init original dataset .csv meta file
+        path = write_csv_meta(original_ds_df, self.key + ".original")
+        self.meta_sub_path = path
+        
+
+    def filter_by_class(self):
+        """_summary_
+        """
+        # # Load and filter the original meta file
+        df = pd.read_csv(self.meta_sub_path)
+        # Get allowed classes and their mapping
+        class_map = get_post_class_mapping(self.key)
+        # Eliminate classes not in class_map
+        df_filtered = df[df["category"].isin(class_map.keys())]
+        df = df_filtered
+        print(df.head(10))
+        
+        # # Map filtered dataframe to self.df with the right schema
+        # # self.df[C.DF_ID_COL] = range(len(df))
+        self.df[C.DF_NAME_COL] = df["filename"]
+        self.df[C.DF_PATH_COL] = df["filepath"].apply(lambda fp: os.path.join(self.ds_abs_path, fp))
+        self.df[C.DF_LENGTH_COL] = self.df[C.DF_PATH_COL].apply(get_wav_data_length)
+        self.df[C.DF_CLASS_ID_COL] = df["category"].apply(lambda original_classname: class_map[original_classname][C.CLASS_ID])
+        self.df[C.DF_CLASS_NAME_COL] = df["category"].apply(lambda original_classname: class_map[original_classname][C.CLASS_NAME])
+        self.df[C.DF_SUB_DS_NAME_COL] = self.name
+        self.df[C.DF_SUB_DS_ID_COL] = df.index
+
+    def normalize(self):
+        """
+        Currently not implemented
+        """
+        pass
+
+    def create_meta(self):
+        df = pd.read_csv(self.meta_sub_path)
+        path = write_csv_meta(self.df, self.key + ".filtered")
+        _ = write_csv_meta(df, self.key + ".original")
+        self.filtered_meta_path = path
+    
+    def get_filtered_meta_path(self):
+        return self.filtered_meta_path
+
+    def move_files(self):
+        """
+        Currently no need to move files, planned datasets would not be stotage comsuming after extracted  
+        """
+        pass
+    
+# def main():
+#     ds = GAD()
+#     append_empty_mapping_to_config(ds, overwrite=False)
+#     ds.hell_yeah()
+
+# if __name__ == "__main__":
+#     main()
--- python/utils/file_utils.py
+++ python/utils/file_utils.py
@@ -1,6 +1,8 @@
 from json_utils import get_default_class_mapping
 import os
-
+import subprocess
+import zipfile
+import uuid
 
 def is_path(path: str) -> bool:
     """
@@ -25,3 +27,42 @@ def get_filename_without_extension(file: str) -> str:
     Get the filename without extension from the given file path.
     """
     return os.path.splitext(os.path.basename(file))[0]
+
+
+def remove_extensions(file_name):
+    names = file_name.split(".")
+    return names[0]
+
+
+def extract(file_path, output_path=".") -> None:
+    print(f"Extract {file_path} to {output_path}")
+    output_file_path = os.path.join(output_path, remove_extensions(file_path))
+    os.makedirs(output_path, exist_ok=True)
+    if file_path.endswith(".zip"):
+        print("Handling zip file ...")
+        with zipfile.ZipFile(file_path, 'r') as zip_ref:
+            zip_ref.extractall(output_file_path)
+    elif file_path.endswith(".tar.gz"):
+        print("Handling .tar.gz file ...")
+        subprocess.run(["tar", "-xzvf", file_path, "-C", output_file_path])
+    else:
+        print(f"Unknown file format {file_path}")
+
+
+def get_file_name_from_url(url) -> str: 
+    return url.split('/')[-1]
+
+def download(url, file_path=".", file_name=None) -> None:
+    if file_name is None:
+        tmp_name = get_file_name_from_url(url)
+        if (tmp_name == ""):
+            print("Cannot get file name from URL")
+            file_name = str(uuid.uuid4())
+        else:
+            file_name = get_file_name_from_url(url)
+    local_filepath = os.path.join(file_path, file_name)
+    print(f"Download {file_name} to {file_path}")
+    subprocess.run(["wget", url, "-O", local_filepath])
+    print(f"Done downloading, file name: {local_filepath}")
+
+    return local_filepath
