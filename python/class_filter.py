bb6b614d7402a6b6e2db3af1ca29a10e62beb527
Thành <131802697+suyttthideptrai@users.noreply.github.com>
2025-03-23 17:07:30 +0000

    pip dependencies #7

--- python/class_filter.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import pandas as pd
-
-meta_esc_50 = "https://raw.githubusercontent.com/karolpiczak/ESC-50/master/meta/esc50.csv"
-with urllib.request.urlopen(meta_esc_50) as f:
-    esc50_df = pd.read_csv(f)
-classes = sorted(esc50_df['category'].unique())
-print(classes)
