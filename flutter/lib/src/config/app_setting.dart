a057e3381b604e7d6efab8b6bf19c4b425fd0f76
toanlemanh <chen22112k3@gmail.com>
2025-03-28 05:39:19 +0700

    create default setting

---
+++ flutter/lib/src/config/app_setting.dart
@@ -0,0 +1,21 @@
+class AppSettings {
+
+  // Keys for local storage
+  static const String alertSoundKey = 'alertSound';
+  static const String languageKey = 'language';
+
+  // Default values
+  static const double defaultSoundSensitivityLevel = 50.0;
+  static const Map<String, bool> defaultRecognisedSound = {
+    'baby cry':false,
+    'glass breaking':false,
+    'gunshot':false,
+    'scream':false,
+    'siren':false
+  };
+  static const bool defaultVibration = true;
+  static const bool defaultAlertSound = true;
+  static const String defaultOperationMode = 'Normal';
+  static const bool defaultBatterySaverMode = false;
+  static const String defaultLanguage = 'English';
+}
