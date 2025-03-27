6ae0f15e2fc60d3d864a135f237f977dc64e78b5
toanlemanh <chen22112k3@gmail.com>
2025-03-28 05:46:25 +0700

    code generated from flutter

--- flutter/test/widget_test.dart
+++ flutter/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:danger_sound_recognition/main.dart';
+import 'package:danger_sound_recognition/src/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
