af3fd9e70a7f7ae50857447844ec2eab6d802323
toanlemanh <chen22112k3@gmail.com>
2025-06-11 15:46:43 +0700

    update: include CSV utils in service

--- app/src/main/java/org/fit/sra/service/SoundClassifierService.java
+++ app/src/main/java/org/fit/sra/service/SoundClassifierService.java
@@ -8,6 +8,8 @@ import android.util.Log;
 
 import org.fit.sra.R;
 import org.fit.sra.state.AppStateManager;
+import org.fit.sra.util.CsvUtils;
+import org.fit.sra.model.CategoryWithSeverity;
 import org.tensorflow.lite.support.audio.TensorAudio;
 import org.tensorflow.lite.support.label.Category;
 import org.tensorflow.lite.task.audio.classifier.AudioClassifier;
@@ -20,6 +22,7 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
+
 public class SoundClassifierService {
 
   private final AudioClassifier classifier;
@@ -31,16 +34,24 @@ public class SoundClassifierService {
 
   private final AppStateManager stateManager;
 
+ 
+
   public SoundClassifierService(Context context) {
     this.stateManager = AppStateManager.getInstance();
     this.mainHandler = new Handler(Looper.getMainLooper());
 
-    try {
+    
+
+    try { 
+      
       String[] files = context.getAssets().list("models");
+      CsvUtils.readSoundCsv(context);
       this.classifier = AudioClassifier
           .createFromFile(context, "models/yamnet/1.tflite");
+           
 //          .createFromFile(context, "models/yamnet_tweaked/ydr_m_2025_06_10.tflite");
     } catch (IOException e) {
+      
       throw new RuntimeException(e);
     }
     this.audioService = new AudioCaptureService(classifier.createAudioRecord());
@@ -56,17 +67,25 @@ public class SoundClassifierService {
         AudioRecord record = audioService.getRecord();
         tensor.load(record);
         List<Classifications> results = classifier.classify(tensor);
-        List<Category> filtered = new ArrayList<>();
-
+        List<CategoryWithSeverity> filtered = new ArrayList<>();
+        
         for (Classifications c : results) {
+          //System.out.println(c);
           for (Category cat : c.getCategories()) {
+             
               if (cat.getScore() > threshold) {
-                  filtered.add(cat);
+                
+                 int index = cat.getIndex();
+                 String severity = CsvUtils.severityByIndex.getOrDefault(index, "Unknown");
+                  //filtered.add(cat);
+                CategoryWithSeverity wrapped = new CategoryWithSeverity(cat, severity);
+                filtered.add(wrapped);
+                // System.out.println(wrapped);
               }
           }
         }
 
-        filtered.sort((a, b) -> Float.compare(b.getScore(), a.getScore()));
+        filtered.sort((a, b) -> Float.compare(b.getCat().getScore(), a.getCat().getScore()));
 
         // Post to main thread
         mainHandler.post(() -> {
