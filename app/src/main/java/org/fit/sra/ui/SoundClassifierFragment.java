d9a69c6e2b9daa67349bc1030448d6e9330656a5
toanlemanh <chen22112k3@gmail.com>
2025-06-11 15:48:08 +0700

    update: include constraint layout and change color conditionally

--- app/src/main/java/org/fit/sra/ui/SoundClassifierFragment.java
+++ app/src/main/java/org/fit/sra/ui/SoundClassifierFragment.java
@@ -1,4 +1,5 @@
 package org.fit.sra.ui;
+import androidx.core.content.ContextCompat;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -11,13 +12,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import java.util.List;
 import org.fit.sra.R;
 import org.fit.sra.service.SoundClassifierService;
 import org.fit.sra.state.AppStateManager;
 import org.fit.sra.util.CategoryUtils;
 import org.fit.sra.util.CommonUtils;
-import org.tensorflow.lite.support.label.Category;
+import org.fit.sra.model.CategoryWithSeverity;
 
 public class SoundClassifierFragment
     extends Fragment
@@ -25,6 +27,7 @@ public class SoundClassifierFragment
                AppStateManager.CategoryStateListener {
 
   private TextView tvClassifier, tvStatus;
+  private ConstraintLayout clClassifier;
   private Button bRecord;
   private AppStateManager stateManager;
 
@@ -53,6 +56,7 @@ public class SoundClassifierFragment
 
     tvStatus = view.findViewById(R.id.tvStatus);
     tvClassifier = view.findViewById(R.id.tvClassifier);
+    clClassifier = view.findViewById(R.id.my_container);
     bRecord = view.findViewById(R.id.bRecord);
 
     // UI Mode theming
@@ -88,13 +92,37 @@ public class SoundClassifierFragment
       bRecord.setText(R.string.button_initial);
       tvStatus.setText(R.string.status_initial);
       tvClassifier.setText(R.string.classifier_initial);
+      clClassifier.setBackgroundColor(Color.parseColor("#FFFFFF"));
     }
   }
 
   private void updateCategoriesUI() {
     if (stateManager.isRecording()) {
-      List<Category> categories = stateManager.getRecognitionCategories();
+      List<CategoryWithSeverity> categories = stateManager.getRecognitionCategories();
       String outputText = CategoryUtils.convertString(categories);
+      String severity = CategoryUtils.getSeverity(categories);
+
+      int color;
+      switch (severity.toUpperCase()) {
+          case "NONE":
+            color = ContextCompat.getColor(requireContext(), R.color.severity_none); // gray from colors.xml
+          break;
+          case "LOW":
+          color = ContextCompat.getColor(requireContext(), R.color.severity_low); // green
+          break;
+          case "MEDIUM":
+          color = ContextCompat.getColor(requireContext(), R.color.severity_medium); // yellow
+          break;
+          case "HIGH":
+          color = ContextCompat.getColor(requireContext(), R.color.severity_high); // red
+          break;
+          default:
+          color = ContextCompat.getColor(requireContext(), R.color.severity_others);// blue gray
+          break;
+      }
+
+      // clClassifier.setBackgroundColor(Color.parseColor("#90A4AE"));
+      clClassifier.setBackgroundColor(color);
       tvClassifier.setText(outputText);
     }
   }
@@ -120,7 +148,7 @@ public class SoundClassifierFragment
   }
 
   @Override
-  public void onCategoryStateChanged(List<Category> categories) {
+  public void onCategoryStateChanged(List<CategoryWithSeverity> categories) {
     String opStr = "";
     if (!CommonUtils.isListNullOrEmpty(categories)) {
       opStr = categories.toString();
@@ -133,4 +161,5 @@ public class SoundClassifierFragment
     SoundClassifierService getSoundClassifierService();
     org.fit.sra.service.PermissionService getPermissionService();
   }
+
 }
